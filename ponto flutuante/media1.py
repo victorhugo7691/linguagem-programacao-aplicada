ee203c986441e1ad7f918249416bedb65af91511
Victor Hugo <victor-f250@hotmail.com>
2023-08-09 00:48:14 -0300

    Media

--- ponto flutuante/media1.py
+++ ponto flutuante/media1.py
@@ -2,10 +2,10 @@ A = float(input())
 B = float(input())
 
 if A>10:
-    A = 10
+    A = 10.0
 elif B>10:
-    B=10
+    B=10.0
 
-nf = A*3.5/10 + B*7.5/10
+nf = A*3.5/11 + B*7.5/11
 
-print(f"MEDIA = {nf: .5f}")
+print(f"MEDIA = {nf: .5f}\n")
