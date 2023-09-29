a3cd69f3b8e01b9718394464f6e2b24ae20ad5b7
Victor Hugo B. S. Miranda <68832758+victorhugo7691@users.noreply.github.com>
2023-09-29 18:58:37 -0300

    Update corrida-de-marrecos.py

--- funcoes/corrida-de-marrecos.py
+++ funcoes/corrida-de-marrecos.py
@@ -1,19 +1,25 @@
-def calcular_numero_corridas(numero_marrecos):
-    numero_corridas = 0
-    
-    while numero_marrecos > 1:
-        if numero_marrecos % 3 == 0:
-            numero_corridas += numero_marrecos // 3
-            numero_marrecos = numero_marrecos // 3
-        elif numero_marrecos % 3 == 1:
-            numero_corridas += (numero_marrecos - 1) // 3
-            numero_marrecos = (numero_marrecos - 1) // 3 + 1
-        else:
-            numero_corridas += (numero_marrecos - 2) // 3
-            numero_marrecos = (numero_marrecos - 2) // 3 + 1
-    
-    return numero_corridas
+def calcular_corridas(participantes):
+    if participantes < 2:
+        return 0
 
-numero_marrecos = int(input())
-numero_corridas = calcular_numero_corridas(numero_marrecos)
-print(numero_corridas)
+    connidas = 0
+
+    while participantes >1:
+
+        if participantes % 3 == 0:
+
+            participantes //= 3 
+        else:
+            participantes = participantes // 3 + 1
+
+        connidas += participantes
+
+    return connidas
+
+while True:
+    t_participantes = int(input())
+    
+    if t_participantes == 0:
+        break 
+
+    print(calcular_corridas(t_participantes))
